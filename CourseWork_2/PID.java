7f0787fbfc78c0cba63e540ae8b1ece4569abb5d
Seokhwan Jung <48024026+ejungsh96@users.noreply.github.com>
2019-11-24 15:13:11 +0000

    Add files via upload

---
+++ CourseWork_2/PID.java
@@ -0,0 +1,51 @@
+package a;
+import lejos.hardware.lcd.LCD;
+
+public class PID {
+	Helper util;
+	Robot wallz;
+	
+	
+	
+	//PID setup Line following
+	float error;
+	float integral = 0; //accumulated error
+	float lastError = 0; //store the last error to be used to calculate the derivative
+	float pidValue = 0;
+	
+	int baseSpeed = 200;
+	float kp = 1.7f;
+	float ki = 0;
+	float kd = 20;
+	float damping = 0.8f;
+	
+	public PID(Robot robot, Helper help){
+		wallz = robot;
+		util = help;
+	}
+	public void run(){
+		LCD.drawString("Left: " + Math.round(wallz.pollSensorLeft()*100) +"          ", 0, 5);
+		LCD.drawString("Right: " + Math.round(wallz.pollSensorRight()*100) +"          ", 0, 6);
+		LCD.drawString("PID: " + pidValue +"          ", 0, 7);
+		error = (wallz.pollSensorLeft() - wallz.pollSensorRight())*100;
+		// Anti Wind-Up
+		if (Math.abs(error) <= 5 || util.oppositeSigns((int)error, (int)lastError)){
+			integral=0;
+		}
+		else{
+			integral = (damping * integral) + error; //update accumulated error, Dampen by multiplying by 2/3
+		}
+		
+		pidValue = (error * kp) + (integral * ki) + ((error - lastError) * kd);
+		wallz.drive(baseSpeed + pidValue, baseSpeed - pidValue);
+		lastError = error;
+	}
+	
+	public void setVals(){
+		baseSpeed = (int)util.inputLCD("Base Speed", 10, (float) baseSpeed, 0);
+		kp = util.inputLCD("Kp", 0.1f, kp, 1);
+		ki = util.inputLCD("Ki", 0.05f, ki, 2);
+		kd = util.inputLCD("Kd", 0.5f, kd, 3);
+		kd = util.inputLCD("damping", 0.01f, damping, 3);
+	}
+}
