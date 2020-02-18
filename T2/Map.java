358b8d55d2d4800c6be409a12faf33d82c8f1d31
Seokhwan Jung <48024026+ejungsh96@users.noreply.github.com>
2020-02-18 13:28:52 +0000

    Add files via upload

---
+++ T2/Map.java
@@ -0,0 +1,16 @@
+package T2;
+
+import java.util.List;
+
+class Map {
+	Node[][] map;
+	public Map(){
+		map = new Node[9][9]		
+	}
+	
+	public void addNode(Node node){
+		map.add(node);
+	}
+	
+	
+}
