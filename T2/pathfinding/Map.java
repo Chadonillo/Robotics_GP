dbae0156af75c57c00fa747ec43bd9ae903f8ab0
plainjames <55738217+plainjames@users.noreply.github.com>
2020-03-25 21:13:56 +0000

    Update Map.java

--- T2/pathfinding/Map.java
+++ T2/pathfinding/Map.java
@@ -3,31 +3,31 @@ package pathfinding;
 //Author of comments does not neccasarily imply the physical author of the code.
 import java.util.List;
 
-//@J This class encodes the map as a series of nodes to be used within the A* pathfinding class. 
+// James: This class encodes the map as a series of nodes to be used within the A* pathfinding class. 
 //In essence, the map is a grid derived from dimensions that correlate to the square arena given
-//in the task. The concept can be visualised by imagining a grid of nodes, with "blocks" - meaning blocked 
+//in the task. The concept can be visualised by imagining a grid network of online nodes and offline
 //nodes, aka places where Wall-z is unable to go and are not included for pathfinding. 
 
-//The locations on the grid such as walls, objects and obstacles are blocked 
-//by adding the corresponding node to the blocks array (A* will then parse this blocked list
-//and set the boolean blocked status of the node to false. Blocked nodes signify places we do not want Wall-z to go or
+//The locations on the grid such as walls, objects and obstacles are represented by offline nodes.
+//I.e. these nodes are offline, are not considered by AStar and should not be visited by Wall-Z.
+//By adding the corresponding node to the offline array (A* will not consider offline nodes)
+//and set the boolean offline status of the node to false. Blocked nodes signify places we do not want Wall-Z to go or
 //collide with. Hence when we feed this map into the A* pathfinding class, a valid route is retrived 
 //in which Wall-Z behaves and traverses the arena as desired and required. 
 
 //Another feature of this class is that it permits a variety of routes to be dynamically generated
 //as required, E.g. once Wall-Z learns information about the arena, such as the
-//position of another obstacle after reading the colour in the box, this map is updated dynamically
-//by appending more blocks to blocked nodes list (i.e. essentially blocking the known obstacle). 
+//position of another obstacle after reading the colour in the box, this map is updated dynamically.
+//By dynamically editing the offline node list we prevent Wall-Z visiting and/or colliding with obstacles, walls, etc.
 
 //Also, once wall-z localises himself on the strip, he proceeds to move onto the nearest node on this
-//map/grid node network. His starting node is noted and used to calculate a route to the known goal
-//which is a specified and constant node on this map. Using various starting (Wall-Z current position)
-//and finishing points Wall-z is able calculate a path with A* and traverse/interact with the arena as
-//required using Brandon's navigation classes.
+//map/grid node network. His starting node is noted and used to calculate an efficient route to any online node on the map
+//Using various starting (Wall-Z current positions)and finishing points Wall-Z is able calculate a path with A* and 
+//traverse/interact with the arena as required using Brandon's navigation classes.
 
 public class Map {
 	private Node[][] mapArea;
-	//@J Define and initialise a list of blocks, e.g. nodes that are not considered in A* pathfinding routes.
+	// J: This method defines and initialises a list of offline nodes, e.g. nodes that are not considered in A* pathfinding routes.
 	private static int[][] offlineNodes = new int[][]{{12, 6}, {12, 9}, {12, 11},
 												{11, 6}, {11, 9}, {11, 12},
 												{10, 5}, {10, 6}, {10, 9}, {10, 13},
@@ -47,11 +47,11 @@ public class Map {
 		generateNodeNetwork();
 		turnNodesOff(offlineNodes);
 	}
-	//@J specifies the map area (13 by 19 blocks).
+	// J: This method specifies the map area (13 by 19 blocks).
 	public Map(){
 		this(13, 19, offlineNodes);
 	}
-	//Go through nodes on map and generate Manhattan Heuristic
+	// J: This method goes through nodes on map and generate their Manhattan Heuristics to a specified destination node
 	public void setHeuristic(Node destination) {
         for (int i = mapArea.length-1; i >= 0 ; i--) {
             for (int j = 0; j < mapArea[0].length; j++) {
@@ -60,7 +60,7 @@ public class Map {
         }
     }
 	
-    //@J the showMap method prints the map/routes to console visually. Very useful in error diagnostic and visualising
+    // J: the showMap methods print the map/routes to console visually. Very useful in error diagnostic and visualising
 	// the map scenario.
 	public void showMap(){
     	for (int i = mapArea.length-1; i >= 0 ; i--){
@@ -102,15 +102,15 @@ public class Map {
 	public Node[][] getMap(){
 		return mapArea;
 	}
-	//@J Turn a specified node offline on the map by setting its offline state to true. Removes nodes consideration by A*.
+	// J: This method brings a specified node offline on the map by setting its offline state to true. Removes nodes consideration by A*.
 	public void setNodeOffline(Node element){
 		this.mapArea[element.getX()][element.getY()].setOffline(true);
 	}
-	//@J Add a specified node back to the map for consideration by A* by setting its offline status state to false.
+	// J: This method brings a specified node back online for consideration by A* by setting its offline status state to false.
 	public void bringNodeOnline(Node element){
 		this.mapArea[element.getX()][element.getY()].setOffline(false);
 	}
-	
+	// J: This method generates THIS maps network of nodes using dimensions specified above
 	private void generateNodeNetwork() {
         for (int i = mapArea.length-1; i >= 0 ; i--) {
             for (int j = 0; j < mapArea[0].length; j++){
@@ -119,7 +119,7 @@ public class Map {
             }
         }
     }
-	//@J Takes nodes from the initial array of specified nodes to be offline and sets the offline status to True.
+	// J: This method takes an array encoding nodes on the map and sets their offline status to True.
 	private void turnNodesOff(int[][] arrayOfNodes) {
         for (int i = 0; i < arrayOfNodes.length; i++) {
             int y = arrayOfNodes[i][0];
